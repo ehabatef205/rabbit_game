9862da75c7da2744951d17bd4a94e67526b8a0ad
Abdallah-S-Abdallah <abdallahsalama808@gmail.com>
2022-12-22 23:36:03 +0200

    mouse moved

--- src/RabbitListener.java
+++ src/RabbitListener.java
@@ -1,6 +1,7 @@
 import javax.media.opengl.GLEventListener;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public abstract class RabbitListener implements GLEventListener, MouseListener {
+public abstract class RabbitListener implements GLEventListener, MouseListener, MouseMotionListener {
     protected String assetsFolderName = "Assets";
 }
