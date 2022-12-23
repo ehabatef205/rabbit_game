93a6145ecb464b90c0f4b0309f4c2c3eac6d9572
Mahmoud Eldeeb <mahmoud.eldib.71@gmail.com>
2022-12-24 01:11:53 +0200

    Moving rabbit , hit at any position ,cleared some redundancy

--- src/RabbitGLEventListener.java
+++ src/RabbitGLEventListener.java
@@ -3,28 +3,39 @@ import com.sun.opengl.util.j2d.TextRenderer;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLCanvas;
 import javax.media.opengl.glu.GLU;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.Random;
 
 public class RabbitGLEventListener extends RabbitListener{
-
+    //till passed by the prior screen
+    int level = 3;
+    float[][] level1 = {{0.5f, 1.0f}, {1.0f, 1.5f}, {1.5f, 1.0f}};
+    float[][] level2 = {{1.5f, 1.5f}, {1.0f, 1.5f}, {0.5f, 1.5f}, {1.5f, 1.1f}, {1.0f, 1.1f}, {0.5f, 1.1f}};
+    float[][] level3 = {{1.5f, 1.7f}, {1.0f, 1.7f}, {0.5f, 1.7f}, {1.5f, 1.3f}, {1.0f, 1.3f}, {0.5f, 1.3f}, {1.5f, 0.9f}, {1.0f, 0.9f}, {0.5f, 0.9f}};
+    float rabbit_displacement=0.18f;
+    int rabbit_position=0;
+    int Hitanimation =0;
+    int rabbit_TTl=0;
     int pit = 2;
     int rabbit = 3;
-    int hammer = 0, hammer2 = 1;
+    int bang =6;
+    int hammer = 0;
     int game_over = 5;
     int xPosition ;
     int yPosition;
-    int maxWidth = 100;
-    int maxHeight = 100;
-    int x = maxWidth / 2, y = maxHeight / 2;
+
+
     int score = 0;
-    int timer = 1000;
+
+    //to be adjusted with frame rate
+    int timer = 100;
+    Random rand = new Random();
     TextRenderer textRenderer = new TextRenderer(new Font("sanaSerif", Font.BOLD, 10));
     TextRenderer textRenderer1 = new TextRenderer(new Font("sanaSerif", Font.BOLD, 40));
-    String textureNames[] = {"hammer.png", "hammer2.png" ,"pit.png", "rabbit.png","back.png", "game_over.png"};
+    String textureNames[] = {"hammer.png", "hammer2.png" ,"pit.png", "rabbit.png","back.png", "game_over.png","boom.png"};
 
     TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
 
@@ -42,7 +53,7 @@ public class RabbitGLEventListener extends RabbitListener{
                 texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i] , true);
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
 
-//                mipmapsFromPNG(gl, new GLU(), texture[i]);
+//              mipmapsFromPNG(gl, new GLU(), texture[i]);
                 new GLU().gluBuild2DMipmaps(
                         GL.GL_TEXTURE_2D,
                         GL.GL_RGBA, // Internal Texel Format,
@@ -63,54 +74,57 @@ public class RabbitGLEventListener extends RabbitListener{
         GL gl = glAutoDrawable.getGL();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);       //Clear The Screen And The Depth Buffer
         gl.glLoadIdentity();
-
-
         DrawBackground(gl);
+        if (timer>0) {
+            if (rabbit_TTl==35) {
+                rabbit_position = rand.nextInt(level * 3);
+            }
+            switch (level) {
+                case 1:
+                    drawaction(gl, level1);
+                    break;
+                case 2:
+                    drawaction(gl, level2);
+                    break;
+                case 3:
+                    drawaction(gl, level3);
+                    break;
+            }
+            DrawScore();
+            if (Hitanimation >0){
+                Hitanimation--;
 
-        //level 1
-        /*
-        DrawPit(gl, x, y, pit, 0.5f, 1.0f,1);
-        DrawPit(gl, x, y, pit, 1.0f, 1.5f,1);
-        DrawPit(gl, x, y, pit, 1.5f, 1.0f,1);
-        */
+                DrawFixed(gl, bang, level3[rabbit_position][0] , level3[rabbit_position][1]-rabbit_displacement , 1);
+            }
+            DrawHammer(gl, xPosition, yPosition, hammer, 0.8f);
+            timer--;
+            rabbit_TTl--;
+            if (rabbit_TTl<-15)
+                rabbit_TTl=35;
+            DrawScore();
+        }
 
-        //level 2
-        /*DrawPit(gl , x , y , pit , 1.5f , 1.2f , 1);
-        DrawPit(gl , x , y , pit , 1.0f , 1.2f , 1);
-        DrawPit(gl , x , y , pit , 0.5f , 1.2f , 1);
-        DrawPit(gl , x , y , pit , 1.5f , 0.8f , 1);
-        DrawPit(gl , x , y , pit , 1.0f , 0.8f , 1);
-        DrawPit(gl , x , y , pit , 0.5f , 0.8f , 1);*/
-
-        //level 3
-        /*DrawPit(gl , x , y , pit , 1.5f , 1.4f , 1);
-        DrawPit(gl , x , y , pit , 1.0f , 1.4f , 1);
-        DrawPit(gl , x , y , pit , 0.5f , 1.4f , 1);
-        DrawPit(gl , x , y , pit , 1.5f , 1.0f , 1);
-        DrawPit(gl , x , y , pit , 1.0f , 1.0f , 1);
-        DrawPit(gl , x , y , pit , 0.5f , 1.0f , 1);
-        DrawPit(gl , x , y , pit , 1.5f , 0.6f , 1);
-        DrawPit(gl , x , y , pit , 1.0f , 0.6f , 1);
-        DrawPit(gl , x , y , pit , 0.5f , 0.6f , 1);*/
-
-        //DrawRabbit(gl, x, y, rabbit, 0.5f , 0.8f , 1);
-        //draw hammer
-        //DrawHammer(gl , xPosition , yPosition , hammer, 0.8f);
-
-        //draw score
-        //DrawScore();
-
-        //draw game_over
-        DrawGameOver(gl, x , y , game_over ,2.5f);
+        else {
+            //draw game_over
+        DrawGameOver(gl, game_over ,2.5f);
         DrawScore_game_over();
+        }
+
+    }
+    void drawaction(GL gl, float[][] level) {
+        for (int i = 0; i < level.length; i++) {
+            DrawFixed(gl,  pit, level[i][0], level[i][1], 1f);
+        }
+        if(rabbit_TTl>0)
+        DrawFixed(gl,rabbit, level[rabbit_position][0] , level[rabbit_position][1]-rabbit_displacement , 1);
     }
 
-    private void DrawGameOver(GL gl, int x, int y, int index, float scale) {
+    private void DrawGameOver(GL gl,  int index, float scale) {
         gl.glEnable(GL.GL_BLEND);
         gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);	// Turn Blending On
 
         gl.glPushMatrix();
-        gl.glTranslated(x / (maxWidth / 2.0) - 1, y / (maxHeight / 2.0) - 0.6, 0);
+        gl.glTranslated(0, 1 - 0.6, 0);
         gl.glScaled(0.2 * scale, 0.2 * scale, 1);
         gl.glBegin(GL.GL_QUADS);
         // Front Face
@@ -139,7 +153,7 @@ public class RabbitGLEventListener extends RabbitListener{
         textRenderer.beginRendering(300, 300);
         textRenderer.setColor(Color.blue);
         textRenderer.draw("Score : " + score, 20, 280);
-        textRenderer.draw("Timer : " + timer, 20, 265);
+        textRenderer.draw("Timer : " + timer/30, 20, 265);
         textRenderer.setColor(Color.WHITE);
         textRenderer.endRendering();
     }
@@ -149,7 +163,7 @@ public class RabbitGLEventListener extends RabbitListener{
         gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);	// Turn Blending On
 
         gl.glPushMatrix();
-        gl.glTranslated(x / (maxWidth / 2.0) - 1, y / (maxHeight / 2.0) - 1, 0);
+        gl.glTranslated(x / (100 / 2.0) - 1, y / (100 / 2.0) - 1, 0);
         gl.glScaled(0.15 * scale, 0.15 * scale, 1);
         gl.glBegin(GL.GL_QUADS);
         // Front Face
@@ -166,12 +180,12 @@ public class RabbitGLEventListener extends RabbitListener{
         gl.glDisable(GL.GL_BLEND);
     }
 
-    public void DrawPit(GL gl , int x , int y , int index ,float x_axis , float y_axis ,  float scale){
+    public void DrawFixed(GL gl , int index ,float x_axis , float y_axis ,  float scale){
         gl.glEnable(GL.GL_BLEND);
         gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);	// Turn Blending On
 
         gl.glPushMatrix();
-        gl.glTranslated(x / (maxWidth / 2.0) - x_axis, y / (maxHeight / 2.0) - y_axis, 0);
+        gl.glTranslated(1- x_axis, 1 - y_axis, 0);
         gl.glScaled(0.2 * scale, 0.2 * scale, 1);
         //System.out.println(x +" " + y);
         gl.glBegin(GL.GL_QUADS);
@@ -190,29 +204,7 @@ public class RabbitGLEventListener extends RabbitListener{
         gl.glDisable(GL.GL_BLEND);
     }
 
-    public void DrawRabbit(GL gl , int x , int y , int index ,float x_axis , float y_axis , float scale){
-        gl.glEnable(GL.GL_BLEND);
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);	// Turn Blending On
 
-        gl.glPushMatrix();
-        gl.glTranslated(x / (maxWidth / 2.0) - x_axis, y / (maxHeight / 2.0) - y_axis, 0);
-        gl.glScaled(0.25 * scale, 0.25 * scale, 1);
-        //System.out.println(x +" " + y);
-        gl.glBegin(GL.GL_QUADS);
-        // Front Face
-        gl.glTexCoord2f(0.0f, 0.0f);
-        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
-        gl.glTexCoord2f(1.0f, 0.0f);
-        gl.glVertex3f(1.0f, -1.0f, -1.0f);
-        gl.glTexCoord2f(1.0f, 1.0f);
-        gl.glVertex3f(1.0f, 1.0f, -1.0f);
-        gl.glTexCoord2f(0.0f, 1.0f);
-        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-        gl.glEnd();
-        gl.glPopMatrix();
-
-        gl.glDisable(GL.GL_BLEND);
-    }
 
     public void DrawBackground(GL gl){
         gl.glEnable(GL.GL_BLEND);
@@ -249,32 +241,22 @@ public class RabbitGLEventListener extends RabbitListener{
     public void mouseClicked(MouseEvent e) {
 
     }
-
+    //// 784 X   761 Y
     @Override
     public void mousePressed(MouseEvent e) {
         double x = e.getX();
         double y = e.getY();
+        System.out.println(x+"    "+y);
+        Hitanimation =10;
+        rabbit_TTl=0;
 
-        Component c = e.getComponent();
-        double width = c.getWidth();
-        double height = c.getHeight();
-
-        xPosition = (int) ((x / width) * 100) + 6;
-        yPosition = ((int) (((height-y) / height) * 100));
         hammer = 1;
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        double x = e.getX();
-        double y = e.getY();
 
-        Component c = e.getComponent();
-        double width = c.getWidth();
-        double height = c.getHeight();
 
-        xPosition = (int) ((x / width) * 100) + 6;
-        yPosition = ((int) (((height-y) / height) * 100));
         hammer = 0;
     }
 
